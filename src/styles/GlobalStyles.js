041549095950743ac4293eda32bc5d86a46f2c3d
bene <matheusbenediht@gmail.com>
2023-05-15 16:25:57 -0300

    Adicionando primeiras cores

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,4 +1,5 @@
 import styled, { createGlobalStyle } from 'styled-components';
+import { primaryColor, primaryDarkColor } from '../config/colors';
 
 export default createGlobalStyle`
   *{
@@ -10,7 +11,8 @@ export default createGlobalStyle`
 
   body{
     font-family: sans-serif;
-    background-color: aliceblue;
+    background-color: ${primaryDarkColor};
+    color: ${primaryDarkColor};
   }
 
   html, body, #root{
@@ -18,9 +20,16 @@ export default createGlobalStyle`
   }
   button{
     cursor: pointer;
+    background-color: ${primaryColor};
+    border: none;
+    color: #fff;
+    padding: 10px;
+    border-radius: 4px;
+    font-weight: 700;
   }
   a{
     text-decoration: none;
+    color: ${primaryColor};
   }
 
   ul{

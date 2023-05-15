668d1e6d87ab6661105d8c5d5dd39e5f4b5bdedc
bene <matheusbenediht@gmail.com>
2023-05-15 16:15:59 -0300

    Começando a criar os Styles, usando styled-components

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,12 +24,9 @@ module.exports = {
   },
   plugins: ["react", "prettier", "react-hooks"],
   rules: {
-    "prettier/prettier": [
-      "error",
-      {
-        endOfLine: "auto"
-      }
-    ],
+    "prettier/prettier": ["error", {
+      "endOfLine":"auto"
+    }],
     "react/jsx-filename-extension": 0,
     "import/prefer-default-export": 0,
     "react-hooks/rules-of-hooks": "error",

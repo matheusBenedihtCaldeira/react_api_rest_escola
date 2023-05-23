b5882cb99991b163ee5aafb1f572c68ccbe7ca96
bene <matheusbenediht@gmail.com>
2023-05-23 18:55:46 -0300

    Adicionando Logout

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,16 +7,17 @@ import {
   FaPowerOff,
 } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Nav } from './styled';
 import * as actions from '../../store/modules/auth/actions';
 import history from '../../services/history';
 
 export default function Header() {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const dispatch = useDispatch();
   const handleLogout = (e) => {
     e.preventDefault();
-    actions.loginFailure();
+    dispatch(actions.loginFailure());
     history.push('/');
   };
   return (
